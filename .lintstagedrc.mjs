78f4e2fc6cdea495e9243b068b976d3180f33c75
Rendi Rizki <rendirizkir@gmail.com>
2025-06-01 16:24:50 +0700

    chore: add husky & lint-staged

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -4,6 +4,7 @@ import { relative } from 'path';
 const buildEslintCommand = (filenames) =>
   `next lint --fix --file ${filenames.map((f) => relative(process.cwd(), f)).join(' --file ')}`;
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
   '*.{js,jsx,ts,tsx}': [buildEslintCommand],
 };
